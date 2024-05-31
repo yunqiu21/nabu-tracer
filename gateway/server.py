280254d66682725e7e876b3145af8a8faa219e2e
Yunqiu (Rachel) Han <60632502+yunqiu21@users.noreply.github.com>
2024-05-30 20:53:39 -0700

    Update Gateway Server (#6)
    * Update Gateway Server
    
    * Stream GET responses to frontend
    * Include node id, trace, time taken when displaying retrieved content
    * Use round robin to select node for GET requests
    * Set peer URLs
    
    * clear screen before loading response
    
    * start timer in GET route; fix index.html to display newline correctly

--- gateway/server.py
+++ gateway/server.py
@@ -1,8 +1,9 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, Response
 from concurrent.futures import ThreadPoolExecutor, as_completed
 import requests
 import random
 from google.cloud import firestore
+import time
 
 app = Flask(__name__)
 
@@ -11,92 +12,122 @@ db = firestore.Client()
 
 # List of IPFS node addresses
 IPFS_URL = [
-    'http://localhost:5001',
-    # TODO: Add more IPFS node addresses
+    "http://10.128.0.2:5000",  # nabu-0
+    "http://10.148.0.2:5000",  # nabu-1
+    "http://10.166.0.2:5000",  # nabu-2
+    "http://10.198.0.2:5000",  # nabu-3
+    "http://10.162.0.2:5000",  # nabu-4
+    "http://10.190.0.2:5000",  # nabu-5
+    "http://10.208.0.3:5000",  # nabu-6
+    "http://10.174.0.2:5000",  # nabu-7
+    "http://10.202.0.2:5000",  # nabu-8
+    "http://10.200.0.2:5000",  # nabu-9
 ]
 
+IPFS_PUT_NODE = 6  # Hardcoded for now to a responsive node.
+IPFS_GET_NODE = 0  # Update with each GET request
+
 # IPFS routes
-PUT_ROUTE = '/api/v0/block/put'
-GET_ROUTE = '/api/v0/block/get'
+PUT_ROUTE = "/api/v0/block/put"
+GET_ROUTE = "/api/v0/block/get"
+
 
 # Web UI
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
+
 
 # Forward GET request to IPFS peer to retrieve all content
-@app.route('/ipfs', methods=['GET'])
+@app.route("/ipfs", methods=["GET"])
 def get_ipfs_content():
-    # Retrieve CIDs from Firestore
-    cid_records = db.collection('cid').stream()
-    cid_list = [record.to_dict()['cid'] for record in cid_records]
-    # Sample CIDs for tracing
-    nsamples = max(1, len(cid_list) // 10)
-    traced = random.sample(range(len(cid_list)), nsamples)
+    def generate():
+        # Retrieve CIDs from Firestore
+        cid_records = db.collection("cid").stream()
+        cid_list = [record.to_dict()["cid"] for record in cid_records]
+        # Sample CIDs for tracing
+        nsamples = max(1, len(cid_list) // 10)
+        traced = random.sample(range(len(cid_list)), nsamples)
+        start_time = time.time()
+        with ThreadPoolExecutor() as executor:
+            future_to_cid = {
+                executor.submit(send_single_get_request, cid, i in traced, start_time): cid
+                for i, cid in enumerate(cid_list)
+            }
+            for future in as_completed(future_to_cid):
+                cid = future_to_cid[future]
+                try:
+                    status, response, node, trace, time_taken = future.result()
+                    if status != 200:
+                        yield f'data: {{"error": "{response}", "node": "nabu-{node}", "trace": "{trace}", "time_taken": "{time_taken:.2f}s"}}\n\n'
+                    else:
+                        # Escape newlines
+                        escaped_response = response.replace("\n", "\\n").replace("\r", "\\r")
+                        yield f'data: {{"content": "{escaped_response}", "node": "nabu-{node}", "trace": "{trace}", "time_taken": "{time_taken:.2f}s"}}\n\n'
+                except Exception as e:
+                    yield f'data: {{"error": "{str(e)}", "node": "nabu-{node}", "trace": "{trace}", "time_taken": "N/A"}}\n\n'
 
-    responses = []
+    return Response(generate(), mimetype="text/event-stream")
 
-    with ThreadPoolExecutor() as executor:
-        future_to_cid = {executor.submit(send_single_get_request, cid, i in traced): cid for i, cid in enumerate(cid_list)}
-        for future in as_completed(future_to_cid):
-            cid = future_to_cid[future]
-            try:
-                status, response = future.result()
-                if status != 200:
-                    responses.append({'error': response})
-                else:
-                    responses.append({'content': response})
-            except Exception as e:
-                responses.append({'error': str(e)})
-
-    return jsonify({"content": responses})
 
 # Forward PUT request to IPFS peer
-@app.route('/ipfs', methods=['PUT'])
+@app.route("/ipfs", methods=["PUT"])
 def put_ipfs_content():
-    # TODO: use round-robin to choose the peer
-    ipfs_node_idx = 0
-    url = IPFS_URL[ipfs_node_idx] + PUT_ROUTE
-    # TODO: support other input formats
-    payload = request.data.decode('utf-8')
+    # TODO: For PUT request, maybe can try all nodes until one succeeds?
+    url = IPFS_URL[IPFS_PUT_NODE] + PUT_ROUTE
+    payload = request.data.decode("utf-8")
 
     if not url:
-        return jsonify({'error': 'URL is required'}), 400
+        return jsonify({"error": "URL is required"}), 400
 
     try:
         response = requests.put(url, data=payload)
         response.raise_for_status()
-        cid = response.json().get('cid')
+        cid = response.json().get("cid")
         if not cid:
-            return jsonify({'error': 'Failed to retrieve CID from response'}), 500
+            return jsonify({"error": "Failed to retrieve CID from response"}), 500
 
         # Store CID to Firestore
-        db.collection('cid').add({'cid': cid})
+        db.collection("cid").add({"cid": cid})
 
-        return jsonify({
-            'content': cid
-        }), response.status_code
+        return jsonify({"content": cid}), response.status_code
 
     except requests.RequestException as e:
-        return jsonify({'error': str(e)}), 500
+        return jsonify({"error": str(e)}), 500
 
-def send_single_get_request(content, trace=False):
-    ipfs_node_idx = 0
-    url = IPFS_URL[ipfs_node_idx] + GET_ROUTE + f"?cid={content}"
+
+def send_single_get_request(content, trace, start_time):
+    global IPFS_GET_NODE
+    node = IPFS_GET_NODE
+    IPFS_GET_NODE = (IPFS_GET_NODE + 1) % len(IPFS_URL)
+    url = IPFS_URL[node] + GET_ROUTE + f"?cid={content}"
+
     if trace:
         url += "&trace=1"
+        # TODO: mark the CID as sampled in datastore
+
+    # debug
+    print(url)
 
     if not url:
-        return 400, 'URL is required'
+        return 400, "URL is required", None, None, None
 
     try:
         response = requests.get(url)
         response.raise_for_status()
-        # TODO: reply with bytestream instead of text
-        return response.status_code, response.text
+        time_taken = time.time() - start_time
+        return response.status_code, response.text, node, trace, time_taken
 
     except requests.RequestException as e:
-        return e.response.status_code if e.response else 500, str(e)
+        time_taken = time.time() - start_time
+        return (
+            e.response.status_code if e.response else 500,
+            str(e),
+            node,
+            trace,
+            time_taken,
+        )
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=80)
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=80)
