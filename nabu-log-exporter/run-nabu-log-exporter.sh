37ff94152bce04a5f6c87aebabf0ba05a8c88f4b
Marshall Miller <marshall@Marshalls-MacBook-Air.local>
2024-06-03 16:34:32 -0700

    Update run script

--- nabu-log-exporter/run-nabu-log-exporter.sh
+++ nabu-log-exporter/run-nabu-log-exporter.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export NABU_TRACING_LOG_PATH=/home/iamsushantgupta/.ipfs/3
-export LOG_EXPORTER_STATE_PATH=/home/iamsushantgupta/.ipfs/3/state/
+export NABU_TRACING_LOG_PATH=/home/iamsushantgupta/.ipfs/
+export LOG_EXPORTER_STATE_PATH=/home/iamsushantgupta/.ipfs/state/
 sudo -E java -cp target/nabu-log-exporter-1.0-SNAPSHOT.jar com.github.millerm.NabuLogExporter
